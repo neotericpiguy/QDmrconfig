8fa9d527b654d0dc9746dcbeb65d1ba08fb9eb7f
Jonathan <neotericpiguy@gmail.com>
2020-12-19 23:16:36 -0700

    Better Makefile

--- src/common/ConfFile.cpp
+++ src/common/ConfFile.cpp
@@ -1,6 +1,25 @@
 #include "ConfFile.hpp"
 
-#include "QDmrconfigEditor.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+extern "C" const char version[] = VERSION;
+const char* copyright;
+
+extern char* optarg;
+extern int optind;
+
+extern "C" void radio_connect(int);
+extern "C" void radio_download(void);
+extern "C" void radio_print_version(FILE* out);
+extern "C" void radio_print_version(FILE* out);
+extern "C" void radio_disconnect(void);
+extern "C" void radio_save_image(const char* filename);
+extern "C" void radio_print_config(FILE* out, int verbose);
+extern "C" void radio_parse_config(const char* filename);
+extern "C" void radio_verify_config();
+extern "C" void radio_upload(int cont_flag);
 
 ConfFile::ConfFile() :
     _filename(),
